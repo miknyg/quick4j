d2c862fcb1502a1d7dc5aa1d88c627ca4f64fad6
Mikael Nygård <mny@kth.se>
2012-09-07 14:21:28 +0200

    refactor

--- src/main/java/se/mikipedia/quick4j/App.java
+++ src/main/java/se/mikipedia/quick4j/App.java
@@ -1,4 +1,4 @@
-package se.mikipedia;
+package se.mikipedia.quick4j;
 
 /**
  * Hello world!
--- src/test/java/se/mikipedia/quick4j/AppTest.java
+++ src/test/java/se/mikipedia/quick4j/AppTest.java
@@ -1,4 +1,4 @@
-package se.mikipedia;
+package se.mikipedia.quick4j;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
